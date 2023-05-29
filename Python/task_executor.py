6c7af9b8b60da85b413c3ccd6e31f57b709a188c
Egor Kostyunin <kostyunin.eg@yandex.ru>
2023-05-29 20:12:17 +0300

    ---

--- Python/task_executor.py
+++ Python/task_executor.py
@@ -1,5 +1,13 @@
 from ctypes import *
 
+class pfarg(Union):
+	_fields_ = [("intNumber", c_long),
+                ("realNumber", c_double),
+                ("strText", c_char_p),
+				("rndData", c_void_p)]
+				
+PFARGPTR = POINTER(pfarg)
+
 plugin = CDLL("../plugin.so")
 
 task_executor = CDLL("../task_executor.so")
